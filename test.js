9d5ed3c133a0de195ecb28ee2c0079683d2ba8b2
Mohammed Rajeh <bwog33@gmail.com>
2024-07-15 18:50:16 +0300

    التعديل الاول اثناء المذاكرة

---
+++ test.js
@@ -0,0 +1,379 @@
+// //lesson 1- 9
+// // {
+// // document.querySelector('p').style.color = "blue"
+// // }
+// // kkkkkk
+
+// // window.alert("hello world");
+
+// // window.alert("hello ali");
+// // document.write("<h2>heelo hamode </h2>");
+// // console.log("hi");
+// // console.error("error");
+// // console.table(["o", "A" ,"K"]);
+// // console.log("heelo hamod" , "color : red; font-size: 32px");
+
+// // q2 
+// document.write("<h1>Elzero</h1>");
+// document.querySelector('h1').style.color = "blue";
+// document.querySelector('h1').style.fontSize = "80px";
+// document.querySelector('h1').style.textAlign = "center";
+// document.querySelector('h1').style.fontFamily = "Arail";
+
+
+// //q3
+// console.log("%celzero %cweb %cschool" , "color : red; font-size: 40px; font-wigth: bold" ,  
+// "color : green; font-size: 40px; font-wigth: bold;",
+// "color:white; font-size: 40px; background-color:blue;");
+
+// //q4
+// console.group('group 1');
+// console.log('message one');
+// console.log('message tow');
+// console.group('child group');
+// console.log('message one');
+// console.log('message tow');
+// console.group('grand child group');
+// console.log('message one');
+// console.log('message tow');
+// console.groupEnd();
+// console.groupEnd();
+// console.groupEnd();
+// console.group('group 2');
+// console.log('message one');
+// console.log('message tow');
+// console.groupEnd();
+
+// //q5
+// console.table(["elzero", "Ahmed" ,"sameh","jamal","aya"]);
+
+//q6
+
+// console.log('Iam in console');
+// document.write("Iam in page");
+/*
+console.log('Iam in console');
+document.write("Iam in page");
+*/
+
+
+
+//lesson 10 - 17
+// console.log(typeof("Mohammed rajeh"));
+// console.log(typeof 555);
+// console.log(typeof 555.22 );
+// console.log(typeof [10 ,15 , 7]); // array
+// console.log(typeof {name : "ali" , age :17 , con : "yemen"}); //object
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null); // object
+
+// var user = "mohammed";
+// var age = 15;
+// console.log(user);
+// console.log(user);
+// console.log(age);
+// console.log(hello);
+
+
+
+//home work 2 
+// q1 
+
+// var numberOne = 10;
+// var number_two = 20;
+
+// console.log(numberOne.toString() + number_two.toString());
+
+// console.log(typeof (numberOne + number_two));
+
+// console.log(`${numberOne}${number_two}`);
+
+// console.log(typeof (`${numberOne}${number_two}`));
+
+// console.log(number_two+"\n"+numberOne);
+
+// console.log(`${number_two}
+// ${numberOne}`);
+
+// //q2
+// console.log(elzero.innerHTML)
+// console.log(typeof elzero)
+// //q3
+// console.log("'i'm In \n \\\\ \n love \\\\ \"\"\" ''' \n ++ with ++ \n \\\"\"\" \\\"\"\" \n \"\"janascript\"\"''" )
+
+//q4
+// الله اعلم انه تحدي مشعة 
+
+// lesson 18 - 
+// var num = 5 ;
+// console.log(10 + 20 );
+// console.log(10 + "osama" );
+// console.log(10 - 20 );
+// console.log(10 - "osama" );
+// console.log(10 ** 5 );
+// console.log(2 % 5 );
+// console.log(35 % 2 );
+// console.log(++num);
+// console.log(num++);
+// console.log(num);
+// console.log(100);
+// console.log(+"100");
+// console.log(+"-100");
+// console.log(+"saleen");
+// console.log(+"55.66");
+// console.log(+false);
+// console.log(Number("55"));
+
+// let a = 4;
+// let b = "50";
+//  let c = true;
+
+// console.log(a + +b);
+// console.log(b - a);
+// console.log(+"");
+// console.log(false - true);
+// console.log(a + c);
+// console.log(a + c + +b);
+// a = a + 20; 
+// a+= 100;
+// a-=50;
+// console.log(a);
+// console.log(1_000_000);
+// console.log(1e6);
+// console.log(10  ** 6);
+// console.log(100000000.0000);
+// console.log(Number.MIN_SAFE_INTEGER);
+// console.log(Number.MIN_VALUE);
+
+
+// console.log((100).toString());
+// console.log(100.10.toString());
+// console.log(100.55355.toFixed(2));
+// console.log(parseInt("100.500"));
+// console.log(parseFloat("100.500"));
+// console.log(Number.isInteger(100.500)); 
+// console.log(Number.isInteger("100.500")); 
+// console.log(Number.isInteger(100)); 
+// console.log(Number.isInteger("ali")); 
+// console.log(Math.round(99.6));
+// console.log(Math.ceil(99.1));
+// console.log(Math.floor(99.9));
+// console.log(Math.min(99,5,5,6));
+// console.log(Math.pow(2,4));
+// console.log(Math.random());
+
+
+
+
+// //assigenment 3 
+// // q2 
+
+// let num = 3 ;
+
+// console.log(num + num );
+// console.log((num * num) - num );
+// console.log(num * --num );
+// console.log(++num + num--);
+// console.log(num + num +  num );
+// console.log(++num + num );
+ 
+
+// //q3 
+// let num2 = 10;
+
+// console.log(num2 + num2 );
+// console.log(num2+=num2 );
+// console.log(num2);
+// console.log(num2+=num2 - num2);
+// console.log((num2*num2)/num2);
+
+// //q4 مدري والله 
+
+
+//assigenment 4
+// q1
+
+// console.log(100_000);
+// console.log(100000);
+// console.log(1e5);
+// console.log(100 *1000);
+// console.log(100 *1000);
+// console.log(5e4 + 5e4);
+// console.log(10e4);
+// console.log(99999+1);
+// console.log(200000/2);
+// console.log(150000-50000);
+// console.log(25000*4);
+// console.log(2e5/2);
+// console.log(1e5 * 1);
+
+// //q2 
+// console.log(Number.MIN_SAFE_INTEGER * -1);
+
+// //q3 مش المطلوب
+// console.log(Number.MAX_SAFE_INTEGER);
+// //q4
+
+// let myVar = "100.56789 viewws";
+
+// console.log(parseInt(myVar));
+// console.log(+parseFloat(myVar).toFixed(2));
+
+// //q5 
+
+// num = 10;
+
+// console.log(Number.isInteger(10) + Number.isInteger(10) );
+
+// //q6 
+// let flt = 10.4 ;
+// console.log(Math.floor(flt));
+// console.log(Math.trunc(flt));
+// console.log(parseInt(flt));
+// console.log(Math.round(flt));
+
+// //q7  الحل غلط شاتأكد منه ذلحين فطور مغرب
+// console.log(isInteger(Math.random(0,4)))
+
+
+
+//
+//  let thename = "  Mohammed";
+//  console.log(thename);
+//  console.log(thename[10]);
+//  console.log(thename.charAt(3));
+//  console.log(thename.charAt(10));
+//  console.log(thename.length);
+//  console.log(thename.trim());
+//  console.log(thename.toUpperCase());
+//  console.log(thename.toLocaleLowerCase());
+//  console.log(thename.trim().charAt(0).toLocaleUpperCase());
+//  let a = "elzero web school";
+//  console.log(a.indexOf("web"));
+//  console.log(a.indexOf("web",9));
+//  console.log(a.lastIndexOf("o"));
+//  console.log(a.slice(-5));
+//  console.log(a.repeat(5));
+//  console.log(a.split());
+//  console.log(a.split(""));
+//  console.log(a.split(" ",2));
+//  console.log(a.substring(2,6));
+//  console.log(a.substring(-10,6));
+//  console.log(a.length);
+//  console.log(a.substring(a.length- 5));
+//  console.log(a.substr(-3));
+//  console.log(a.substr(-5 , 2));
+//  console.log(a.startsWith("e"));
+//  console.log(a.startsWith("e",2));
+//  console.log(a.startsWith("zero",3));
+
+
+
+// lesson 27 - 30 
+// q1 
+// let username = "Elzero" ; 
+// console.log(username);
+// console.log(username[0].toLocaleLowerCase());
+// console.log(username.charAt(0).toLocaleLowerCase());
+// console.log(username.substring(0,1).toLocaleLowerCase());
+// console.log(username.slice(0,1).toLocaleLowerCase());
+// console.log(username.substr(0,1).toLocaleLowerCase());
+// console.log(username.substr(0,1).toLocaleLowerCase().repeat(3));
+
+
+// //q2  
+
+// let word = "Elzero" ;
+// console.log(word.includes("z"));
+// console.log(word.startsWith("E"));
+// console.log(word.endsWith("o"));
+
+
+// lesson 33 - 37
+
+// q1
+let num = 9 ;
+if (num < 10)
+{
+    console.log(`00${num}`)
+}
+else if (num  > 10 && num < 100)
+{
+    console.log(`0${num}`)
+}
+else if (num >= 100)
+{
+    console.log(num)
+    
+}
+
+
+ num = 20 ;
+if (num < 10)
+{
+    console.log(`00${num}`)
+}
+else if (num  > 10 && num < 100)
+{
+    console.log(`0${num}`)
+}
+else if (num >= 100)
+{
+    console.log(num)
+    
+}
+
+num = 110 ;
+if (num < 10)
+{
+    console.log(`00${num}`)
+}
+else if (num  > 10 && num < 100)
+{
+    console.log(`0${num}`)
+}
+else if (num >= 100)
+{
+    console.log(num)
+    
+}
+
+
+//q2 
+let num1 = 9;
+let str = "9";
+let str2 = "20";
+if (num1 == str )
+{
+    console.log("{num1} is The same value as {str}");
+}
+if (num1 === str )
+{
+    console.log("{num1} is The same value as {str} but not  the same type");
+}
+if (num1 !== str2 )
+{
+    console.log("{num1} is not The same value or the same type of {str2}");
+}
+if (typeof str == typeof str2 )
+{
+    console.log("{str} is the same type as {str2} but not the same value ");
+}
+
+let num2 = 10;
+let num3 = 30;
+let num4 = "30";
+
+if (num4 > num2 && num4 !== num3 )
+{
+    console.log("30 is larger 10 and type string not the same number")
+}
+if (num4 > num2 && num4 !== num3 && num4 == num3)
+{
+    console.log("30 is larger 10 and the value is the same as 30 and type string not the same number")
+}
+if (num4 !== num2 && num4 !== num3 )
+{
+    console.log("{num4} value and type is not the same as {num2} and not the same type as {num3}")
+}
